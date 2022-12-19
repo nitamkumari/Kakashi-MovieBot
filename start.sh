a8efcfb1cb1ee5b3d501460e53af3733626d9d77
Xission Proz <115548739+nitamkumari@users.noreply.github.com>
2022-12-19 21:54:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Cyniteofficial/Auto-Filter-V5.git /Auto-Filter-V5
+  git clone https://github.com/nitamkumari/Kakashi-MovieBot.git /Kakashi-MovieBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-Filter-V5
+  git clone $UPSTREAM_REPO /Kakashi-MovieBot
 fi
-cd /Auto-Filter-V5
+cd /Kakashi-MovieBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
